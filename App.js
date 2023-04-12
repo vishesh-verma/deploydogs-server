c22e0b4c2700220509a28509a7bfe4846213b767
vishesh-verma <vishesh.verma2412@gmail.com>
2023-04-12 18:32:44 +0530

    use spawn

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
-const { exec, execSync } = require("child_process");
+const { exec, execSync, spawn } = require("child_process");
 const { log } = require("console");
 
-exec("ls ", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
+
+
+ const child = spawn("cd react-basics",()=>{
+
+ })
+ child.on('npm install', function (code, signal) {
+    console.log('child process exited with ' +
+                `code ${code} and signal ${signal}`);
+  });
+// exec("ls ", (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//     }
+//     if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+// });
 // exec("git clone https://github.com/vishesh-verma/react-basics.git", (error, stdout, stderr) => {
 //     if (error) {
 //         console.log(`error: ${error.message}`);
@@ -23,25 +32,25 @@ exec("ls ", (error, stdout, stderr) => {
 //     }
 //     console.log(`stdout: ${stdout}`);
 // });
-execSync("cd react-basics", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
+// execSync("cd react-basics", (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//     }
+//     if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout}`);
     
-});
-execSync("npm install", (error)=>{
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    console.log("npm installed");
-})
+// });
+// execSync("npm install", (error)=>{
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//     }
+//     console.log("npm installed");
+// })
 
 // exec("cd react-basics & yarn install", (error, stdout, stderr) => {
 //     if (error) {
