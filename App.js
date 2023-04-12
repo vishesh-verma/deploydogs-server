312b308fd6b54ed51d64ee5c5fac6f8a409ba34b
vishesh-verma <vishesh.verma2412@gmail.com>
2023-04-12 17:48:57 +0530

    test1

--- App.js
+++ App.js
@@ -11,17 +11,17 @@ exec("ls ", (error, stdout, stderr) => {
     }
     console.log(`stdout: ${stdout}`);
 });
-// exec("cd react-basics", (error, stdout, stderr) => {
-//     if (error) {
-//         console.log(`error: ${error.message}`);
-//         return;
-//     }
-//     if (stderr) {
-//         console.log(`stderr: ${stderr}`);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout}`);
-// });
+exec("cd react-basics & yarn install", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
 
 // exec("cd react-basics & yarn install", (error, stdout, stderr) => {
 //     if (error) {
