46123b9d8b5cf6cddde47c845c97625258d76000
vishesh-verma <vishesh.verma2412@gmail.com>
2023-04-13 20:55:30 +0530

    fixed npm start issue

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-const { exec, execSync } = require("child_process");
+const { exec, execSync, fork } = require("child_process");
 const { log } = require("console");
 
 // exec("ls ", (error, stdout, stderr) => {
@@ -12,7 +12,18 @@ const { log } = require("console");
 //     }
 //     console.log(`stdout: ${stdout}`);
 // });
-exec("git clone https://github.com/vishesh-verma/react-basics.git", (error, stdout, stderr) => {
+execSync("git clone https://github.com/iCodeWalker/GymWebApp.git && cd GymWebApp  &&  npm install", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    return;
+})
+exec("cd GymWebApp  &&  npm start", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
@@ -22,31 +33,33 @@ exec("git clone https://github.com/vishesh-verma/react-basics.git", (error, stdo
         return;
     }
     console.log(`stdout: ${stdout}`);
-});
-exec("cd react-basics && npm install", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
+})
     
-});
-exec("cd react-basics && npm start", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
+
+// exec("cd react-basics && npm install", (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//     }
+//     if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout}`);
     
-});
+// });
+// exec("cd react-basics && npm start", (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//     }
+//     if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+    
+// });
 // exec("npm install", (error)=>{
 //     if (error) {
 //         console.log(`error: ${error.message}`);
